a4904f52a861212179800d5a0de9fecb1ba77c99
Jay Makwana <110513329+JAYMAKWANA2202@users.noreply.github.com>
2023-04-24 10:46:28 +0530

    few change in code

--- hunting-coder/pages/blog.js
+++ hunting-coder/pages/blog.js
@@ -7,7 +7,29 @@ export default function Blog() {
         <title>Blog Page</title>
         <link rel="icon" href="/coder.png" />
       </Head>
-      <div>blog</div>
+      <div>
+        <div className="deatil">
+          <h3>How to learn React JS in 2023?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            ipsa!
+          </p>
+        </div>
+        <div className="deatil">
+          <h3>How to learn React JS in 2023?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            ipsa!
+          </p>
+        </div>
+        <div className="deatil">
+          <h3>How to learn React JS in 2023?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+            ipsa!
+          </p>
+        </div>
+      </div>
     </>
   );
 }
