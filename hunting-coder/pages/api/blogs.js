a0dcefe9d5c2a7a482500d53c6b2b6f484228962
Jay Makwana <110513329+JAYMAKWANA2202@users.noreply.github.com>
2023-04-24 13:38:38 +0530

    read file and pass data

--- hunting-coder/pages/api/blogs.js
+++ hunting-coder/pages/api/blogs.js
@@ -1,7 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
-export default function handler(req, res) {
-  fs.readFile("blogdata/how-to-learn-javaScript.json", "utf-8", (err, data) => {
-    res.status(200).json(JSON.parse(data));
-  });
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let myfile;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    myfile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+    allBlogs.push(JSON.parse(myfile));
+  }
+  res.status(200).json(allBlogs);
 }
