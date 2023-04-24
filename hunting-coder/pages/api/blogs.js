a9200888d09c9c2fcee8be64869dd17db5b60b32
Jay Makwana <110513329+JAYMAKWANA2202@users.noreply.github.com>
2023-04-24 12:40:08 +0530

    use api to call json file

---
+++ hunting-coder/pages/api/blogs.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from "fs";
+export default function handler(req, res) {
+  fs.readFile("blogdata/how-to-learn-javaScript.json", "utf-8", (err, data) => {
+    res.status(200).json(JSON.parse(data));
+  });
+}
