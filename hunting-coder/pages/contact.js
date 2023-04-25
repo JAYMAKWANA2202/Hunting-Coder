572f4bf2695dd413eb3bac1aba6393c2b38a1907
Jay Makwana <110513329+JAYMAKWANA2202@users.noreply.github.com>
2023-04-25 16:32:20 +0530

    completed the contact page

--- hunting-coder/pages/contact.js
+++ hunting-coder/pages/contact.js
@@ -1,12 +1,53 @@
 import Head from "next/head";
 import React from "react";
 import styles from "../styles/Contact.module.css";
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-};
+import { useState } from "react";
 
 export default function Contact() {
+  const [name, setname] = useState("");
+  const [phone, setphone] = useState("");
+  const [email, setemail] = useState("");
+  const [desc, setdesc] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(name, phone, email, desc);
+    async function postJSON(data) {
+      try {
+        const response = await fetch("http://localhost:3000/api/postcontact/", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log("Success:", result);
+        setname("");
+        setemail("");
+        setphone("");
+        setdesc("");
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    const data = { name, phone, email, desc };
+    postJSON(data);
+  };
+
+  const handlechange = (e) => {
+    if (e.target.name == "name") {
+      setname(e.target.value);
+    } else if (e.target.name == "phone") {
+      setphone(e.target.value);
+    } else if (e.target.name == "email") {
+      setemail(e.target.value);
+    } else if (e.target.name == "desc") {
+      setdesc(e.target.value);
+    }
+  };
   return (
     <>
       <Head>
@@ -24,6 +65,8 @@ export default function Contact() {
               type="text"
               className="form-control"
               id="name"
+              value={name}
+              onChange={handlechange}
               name="name"
               aria-describedby="emailHelp"
             />
@@ -36,17 +79,21 @@ export default function Contact() {
               type="email"
               className="form-control"
               id="email"
+              value={email}
+              onChange={handlechange}
               name="email"
               aria-describedby="emailHelp"
             />
           </div>
           <div className={styles.mb3}>
             <label htmlFor="phone" className={styles.formlabel}>
-              Password
+              Phone
             </label>
             <input
               type="phone"
               className="form-control"
+              value={phone}
+              onChange={handlechange}
               name="phone"
               id="phone"
             />
@@ -56,8 +103,11 @@ export default function Contact() {
             <textarea
               className={styles.mb3}
               placeholder="Leave a comment here"
+              onChange={handlechange}
+              value={desc}
+              name="desc"
               id="floatingTextarea"
-            ></textarea>
+            />
           </div>
           <button type="submit" className="btn btn-primary">
             Submit
